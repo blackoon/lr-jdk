a57278783988b68f6ace8f3db4fd1d5a72bf0cf6
blackoon <zy775858>
2016-05-30 22:18:05 +0800

    set is map

--- src/main/java/com/blackoon/lr/algorithm/mathematicsbeauty/Client.java
+++ src/main/java/com/blackoon/lr/algorithm/mathematicsbeauty/Client.java
@@ -1,5 +1,9 @@
 package com.blackoon.lr.algorithm.mathematicsbeauty;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
 import java.util.Vector;
 
 public class Client {
@@ -9,6 +13,11 @@ public class Client {
 		//线程安全与线程非安全
 		//Set List Quene Map
 		//Map
-		
+		// Iterator 轻量级对象
+//		v.iterator();
+		HashSet hset=null;
+		Set set=null;
+		LinkedHashSet lhs=null;
+		TreeSet treeSet=null;
 	}
 }

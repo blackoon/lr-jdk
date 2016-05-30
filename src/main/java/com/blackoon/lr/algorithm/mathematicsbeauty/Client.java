cc07bee4b8af40c9d7eefd11f9540c97ea208bb6
blackoon <zy775858>
2016-05-30 22:04:29 +0800

    arrayList linkedList Vector hashtable hashMap

--- src/main/java/com/blackoon/lr/algorithm/mathematicsbeauty/Client.java
+++ src/main/java/com/blackoon/lr/algorithm/mathematicsbeauty/Client.java
@@ -1,5 +1,14 @@
 package com.blackoon.lr.algorithm.mathematicsbeauty;
 
-public class Client {
+import java.util.Vector;
 
+public class Client {
+	public static void main(String[] args) {
+		Vector<String> v=null;
+		//Vector&ArrayList、Hashtable&HashMap
+		//线程安全与线程非安全
+		//Set List Quene Map
+		//Map
+		
+	}
 }
